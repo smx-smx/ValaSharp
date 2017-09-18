d8332e28da08ba5b739e006c90715104020a4833
smx-smx <smxdev4@gmail.com>
2017-09-18 22:15:40 +0200

    GLibPorts: Re-enabled and fixed arguments escaping

--- GLibPorts/Shell.cs
+++ GLibPorts/Shell.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace GLibPorts
@@ -9,19 +10,7 @@ namespace GLibPorts
 	public partial class GLib {
 		public class Shell {
 			public static string quote(string str) {
-				//For now we're using commands directly without a shell
-#if false
-				StringBuilder sb = new StringBuilder("'");
-				foreach(char ch in str) {
-					if (ch == '\'')
-						sb.Append("'\\''");
-					else
-						sb.Append(ch);
-				}
-				sb.Append('\'');
-				return sb.ToString();
-#endif
-				return str;
+				return "\"" + Regex.Replace(str, @"(\\+)$", @"$1$1") + "\"";
 			}
 		}
 	}
