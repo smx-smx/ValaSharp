6e5f831dca43e33fbd96cad5a7722e5d272e921b
smx-smx <smxdev4@gmail.com>
2017-09-18 19:57:46 +0200

    Added initial tests project

---
+++ ValaTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ValaTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ValaTests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9fcb3fa8-705e-4596-aca6-7bf6472ef98f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ValaTests/TestsRunner.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ValaTests
+{
+	public static class TestsRunner
+	{
+		public static string ToolchainPath { get; private set; }
+
+		[ClassInitialize]
+		public static void InitializeEnvironment(TestContext ctx)
+		{
+			ToolchainPath = ctx.Properties["toolchain"].ToString();
+		}
+	}
+}
