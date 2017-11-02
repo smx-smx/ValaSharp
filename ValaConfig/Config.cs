3d40f88b5fec201d22458ff9c571f99ad017bf2f
Stefano Moioli <smxdev4@gmail.com>
2017-11-02 01:25:12 +0100

    Fixed default culture (was breaking double parsing on non English cultures)

--- ValaConfig/Config.cs
+++ ValaConfig/Config.cs
@@ -1,7 +1,31 @@
-﻿namespace ValaConfig
+﻿using System;
+using System.Globalization;
+using System.Threading;
+
+namespace ValaConfig
 {
 	public static class Config
 	{
+		private static void SetCCulture()
+		{
+			// Clone the current Culture, and alter it as needed
+			CultureInfo CCulture = Thread.CurrentThread.CurrentCulture.Clone() as CultureInfo;
+			CCulture.NumberFormat.NumberDecimalSeparator = ".";
+			CCulture.NumberFormat.NaNSymbol = "NaN";
+			CCulture.NumberFormat.PositiveInfinitySymbol = "infin";
+			CCulture.NumberFormat.NegativeInfinitySymbol = "-infin";
+
+			// Change the current thread culture
+			Thread.CurrentThread.CurrentCulture = CCulture;
+
+			// Set the new culture for all new threads
+			CultureInfo.DefaultThreadCurrentCulture = CCulture;
+		}
+
+		static Config() {
+			SetCCulture();
+		}
+
 		public static string PACKAGE_SUFFIX { get; set; } = "";
 		public static string PACKAGE_DATADIR { get; set; } = "";
 		public static string BUILD_VERSION { get; set; } = "";
